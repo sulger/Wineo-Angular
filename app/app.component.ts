6cf58c36a151e69b911b0f362dcfbccff721ef04
Snekkja <ferreira.julien@gmail.com>
2016-07-19 15:02:45 +0200

    Ajout d'une liste des caves

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,43 +9,109 @@ import {Bouteille} from "./domain/bouteille";
 
 export class AppComponent {
 
-    // Cave de l'utilisateur
-    public cave: Cave = {
-        nomProprietaire: 'Toto',
-        capaciteMaximale: 100,
-        emplacement: 'AltF4',
-        bouteilles: [
-            {
-                reference: 'KIFJG',
-                designation: 'Chablis',
-                contenance: 0.75,
-                annee: 2014
-            },
-            {
-                reference: 'FHEBIE',
-                designation: 'Champagne',
-                contenance: 1,
-                annee: 2015
-            },
-            {
-                reference: 'EFEFK',
-                designation: 'Bordeaux',
-                contenance: 0.75,
-                annee: 2011
-            }
-        ]
-    }
+    // Ensembles des caves
+    public caves: Cave[] = [
+        {
+            nomProprietaire: 'Toto',
+            capaciteMaximale: 100,
+            emplacement: 'AltF4',
+            bouteilles: [
+                {
+                    reference: 'KIFJG',
+                    designation: 'Chablis',
+                    contenance: 0.75,
+                    annee: 2014
+                },
+                {
+                    reference: 'FHEBIE',
+                    designation: 'Champagne',
+                    contenance: 1,
+                    annee: 2015
+                },
+                {
+                    reference: 'EFEFK',
+                    designation: 'Bordeaux',
+                    contenance: 0.75,
+                    annee: 2011
+                }
+            ]
+        },
+        {
+            nomProprietaire: 'Titi',
+            capaciteMaximale: 25,
+            emplacement: 'F5',
+            bouteilles: [
+                {
+                    reference: 'JDLFJ',
+                    designation: 'Chardonnais',
+                    contenance: 0.75,
+                    annee: 2012
+                },
+                {
+                    reference: 'LYFHGT',
+                    designation: 'Coca-Cola',
+                    contenance: 2,
+                    annee: 2016
+                }
+            ]
+        },
+        {
+            nomProprietaire: 'Michu',
+            capaciteMaximale: 5,
+            emplacement: 'D2',
+            bouteilles: [
+                {
+                    reference: 'MDSCN',
+                    designation: 'Lambrusco',
+                    contenance: 0.75,
+                    annee: 2014
+                },
+                {
+                    reference: 'PCEIEZC',
+                    designation: 'Saint-Julien',
+                    contenance: 0.75,
+                    annee: 1998
+                },
+                {
+                    reference: 'CSCSCR',
+                    designation: 'Bordeaux',
+                    contenance: 0.75,
+                    annee: 2014
+                }
+            ]
+        }
+    ]
 
-    // Bouteille à créer.
-    public nouvelleBouteille: Bouteille = new Bouteille();
+    // Cave sélectionnée dans la liste.
+    public caveSelectionnee: Cave;
 
     // Bouteille sélectionnée dans la liste.
     public bouteilleSelectionnee: Bouteille;
 
+    // Bouteille à créer.
+    public nouvelleBouteille: Bouteille = new Bouteille();
+
+    /**
+     * Ajouter une nouvelle bouteille
+     */
     public ajouterNouvelleBouteille() {
-        this.cave.bouteilles.push(this.nouvelleBouteille);
+        this.caveSelectionnee.bouteilles.push(Object.assign({ }, this.nouvelleBouteille));
     }
 
+    /**
+     * Sélectionner une cave.
+     *
+     * @param cave La cave sélectionnée.
+     */
+    public selectionnerCave(cave: Cave) {
+        this.caveSelectionnee = cave;
+    }
+
+    /**
+     * Sélectionner une bouteille.
+     *
+     * @param bouteille La bouteille sélectionnée.
+     */
     public selectionnerBouteille(bouteille: Bouteille) {
         this.bouteilleSelectionnee = bouteille;
     }
